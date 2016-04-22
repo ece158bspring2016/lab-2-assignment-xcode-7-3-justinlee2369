8c9de56902fabebe96c5ee45c38f6e9da7c937ea
justin.lee2369@gmail.com <justin.lee2369@gmail.com>
2016-04-22 11:49:18 -0700

    finished lab1

---
+++ Lab1/SampleData.swift
@@ -0,0 +1,15 @@
+//
+//  SampleData.swift
+//  Lab1
+//
+//  Created by Justin Lee on 4/21/16.
+//  Copyright © 2016 Justin Lee. All rights reserved.
+//
+
+import Foundation
+//Set up sample data
+
+let playersData = [
+    Player(name:"Bill Evans", game:"Tic-Tac-Toe", rating: 4),
+    Player(name: "Oscar Peterson", game: "Spin the Bottle", rating: 5),
+    Player(name: "Dave Brubeck", game: "Texas Hold 'em Poker", rating: 2) ]
